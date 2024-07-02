cf18db3acc2941c2a3d161ad567276ba94ab1f52
Jorge Coello <44264739+georgecoello@users.noreply.github.com>
2024-07-02 15:10:23 -0600

    trabajado UNAH

---
+++ FirstAdmission.tsx
@@ -0,0 +1,12 @@
+import {FondoA} from "../../components/App/FondoAdmission"
+import './FirstAdmission.css'
+
+export default function Admission(){
+    return (
+        <>
+            <FondoA>
+                <h1 className="title">Admisiones</h1>
+            </FondoA>
+        </>
+    )
+}
